93fc3386d0ac4ba93e85d4350b26bade9dad6575
Mohd Faiz <mohdfaiz.1842@gmail.com>
2024-09-11 10:01:42 +0530

    ad

--- src/config/config.js
+++ src/config/config.js
@@ -6,9 +6,9 @@ let base_url = ''
 
 
 if (mode === production) {
-    base_url = ''
-} else {
     base_url = 'https://mern-architectus-bureau.vercel.app'
+} else {
+    base_url = 'http://localhost:8080'
 }
 
 export {base_url}
