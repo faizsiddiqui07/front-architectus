0a4fa8b3de7596fb033ca8b435275cde11e4c3d4
Mohd Faiz <mohdfaiz.1842@gmail.com>
2024-12-31 13:49:06 +0530

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,4 @@
-const production = 'production'
-const development = 'development'
+//const base_url = 'http://localhost:8080'
+const base_url = 'https://mern-architectus-bureau.vercel.app'
 
-const mode = development
-let base_url = ''
-
-
-if (mode === production) {
-    base_url = 'http://localhost:8080'
-} else {
-    base_url = 'https://mern-architectus-bureau.vercel.app'
-}
-
-export {base_url}
+export { base_url }
