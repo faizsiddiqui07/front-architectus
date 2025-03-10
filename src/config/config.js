744d756f20c91f1edaa7cc5e259dee3efa09c833
Mohd Faiz <mohdfaiz.1842@gmail.com>
2025-03-10 16:49:01 +0530

    add urls

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
-const base_url = 'http://localhost:8080'
+// const base_url = 'http://localhost:8080'
+const base_url = 'https://api.architectusbureau.com/'
 // const base_url = 'https://mern-architectus-bureau.vercel.app'
 
 export { base_url }
