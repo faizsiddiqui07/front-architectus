b1c987170bc5ec967f75478725edd7317ccf7508
Mohd Faiz <mohdfaiz.1842@gmail.com>
2025-05-02 11:21:01 +0530

    add

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 // const base_url = 'http://localhost:8080'
-const base_url = 'https://api.architectusbureau.com/'
+// const base_url = 'https://api.architectusbureau.com/'
 // const base_url = 'https://mern-architectus-bureau.vercel.app'
+const base_url = 'https://k0jff9dzh8.execute-api.ap-south-1.amazonaws.com/'
 
 export { base_url }
